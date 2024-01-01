99ab3fcb93df35f36498883d28d1caf6d88a3253
Ben Kanagroo <78645337+Lenniott@users.noreply.github.com>
2024-01-01 23:53:00 +0000

    updated JSON now has all text formats from medium

--- convertRSS.js
+++ convertRSS.js
@@ -1,8 +1,4 @@
 "use strict";
-// import { parse } from 'rss-to-json';
-// import { HTMLToJSON } from 'html-to-json-parser';
-// import sanitizeHtml from 'sanitize-html';
-// import { writeFileSync } from 'fs';
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -16,32 +12,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// const rssUrl = 'https://medium.com/@benjamin.mizrany/feed';
-// const article = 0;
-// (async () => {
-//     try {
-//         const rss = await parse(rssUrl);
-//         let htmlContent = `<div>${rss.items[article].content}</div>`;
-//         // Sanitize HTML content
-//         htmlContent = sanitizeHtml(htmlContent, {
-//             allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img', 'figure']),
-//             allowedAttributes: false,
-//         });
-//         const json = await HTMLToJSON(htmlContent, true);
-//         rss.items[article].content = json; 
-//         // Save to a JSON file
-//         const fileName = 'articleContent.json';
-//         writeFileSync(fileName, JSON.stringify(rss.items[article], null, 3));
-//         console.log(`Content saved to ${fileName}`);
-//     } catch (error) {
-//         console.error('Error:', error);
-//     }
-// })();
 const rss_to_json_1 = require("rss-to-json");
 const html_to_json_parser_1 = require("html-to-json-parser");
 const sanitize_html_1 = __importDefault(require("sanitize-html"));
 const fs_1 = require("fs");
-const rssUrl = 'https://medium.com/@benjamin.mizrany/feed';
+const userName = "oliver.n.winfield";
+const rssUrl = `https://medium.com/@${userName}/feed`;
 const article = 0;
 (() => __awaiter(void 0, void 0, void 0, function* () {
     try {
--- convertRSS.ts
+++ convertRSS.ts
@@ -1,41 +1,12 @@
-// import { parse } from 'rss-to-json';
-// import { HTMLToJSON } from 'html-to-json-parser';
-// import sanitizeHtml from 'sanitize-html';
-// import { writeFileSync } from 'fs';
 
-// const rssUrl = 'https://medium.com/@benjamin.mizrany/feed';
-// const article = 0;
-
-// (async () => {
-//     try {
-//         const rss = await parse(rssUrl);
-//         let htmlContent = `<div>${rss.items[article].content}</div>`;
-        
-//         // Sanitize HTML content
-//         htmlContent = sanitizeHtml(htmlContent, {
-//             allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img', 'figure']),
-//             allowedAttributes: false,
-//         });
-
-//         const json = await HTMLToJSON(htmlContent, true);
-//         rss.items[article].content = json; 
-
-//         // Save to a JSON file
-//         const fileName = 'articleContent.json';
-//         writeFileSync(fileName, JSON.stringify(rss.items[article], null, 3));
-//         console.log(`Content saved to ${fileName}`);
-
-//     } catch (error) {
-//         console.error('Error:', error);
-//     }
-// })();
 
 import { parse } from 'rss-to-json';
 import { HTMLToJSON } from 'html-to-json-parser';
 import sanitizeHtml from 'sanitize-html';
 import { writeFileSync } from 'fs';
 
-const rssUrl = 'https://medium.com/@benjamin.mizrany/feed';
+const userName = "oliver.n.winfield";
+const rssUrl = `https://medium.com/@${userName}/feed`;
 const article = 0;
 
 (async () => {
